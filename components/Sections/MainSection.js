198ca072843047cabd7eb3ae48096bfc238d3b50
matt6852 <prorokwow@mail.ru>
2021-05-19 16:37:19 +0300

    redone main section navbar according to figma

--- components/Sections/MainSection.js
+++ components/Sections/MainSection.js
@@ -1,32 +1,36 @@
 import style from "styles/MainSection.module.scss";
-import { FaMouse } from "react-icons/fa";
+import Image from "next/image";
+
 import Wrapper from "../Wrapper";
 
 export default function MainSection() {
   return (
     <section className={style.main_section}>
-      {/* <Wrapper>
-        <div className={style.center}>
-          <div className={style.title_container}>
+      <Wrapper>
+        <div className={style.title_center}>
+          <div>
             <h1>
               {" "}
-              <span> Свобода</span> пригорода, <span>комфорт</span> города
+              <span className="linercradient">Свобода</span> пригорода,{" "}
+              <span className="linercradient">комфорт </span> города
             </h1>
           </div>
           <div className={style.btn_container}>
-            <button className={style.btn_main_one}>
-              Записаться на просмотр &rarr;
+            <button>
+              {" "}
+              <a href="">Записаться на просмотр &#8594;</a>{" "}
             </button>
-            <p className={style.btn_main_two}>
+            <p>
               Участки под индивидуальную застройку в живописном районе
               Ленобласти
             </p>
           </div>
+          <div className={style.mous}>
+            <Image src="/mous.png" width={16} height={16} />
+            <p>Листайте ниже</p>
+          </div>
         </div>
-        <div className={style.mous}>
-          <FaMouse className={style.icon} /> <span>Листайте ниже</span>
-        </div>
-      </Wrapper> */}
+      </Wrapper>
     </section>
   );
 }
