008c50ecd62b8ba07fd52a25d9714a2debaf272a
matt6852 <prorokwow@mail.ru>
2021-05-20 16:31:29 +0300

    added history section

--- components/Sections/History.js
+++ components/Sections/History.js
@@ -9,8 +9,56 @@ export default function History() {
         <div className={style.title}>
           <h3>
             <span className="linercradient">История </span>{" "}
-            <span className ="white"> посёлка</span>
+            <span className="white"> посёлка</span>
           </h3>
+        </div>
+        <div className={style.flex_center}>
+          <div className={style.flex_center_subflex_1}>
+            <div className={style.flex_item}>
+              <h3 className="white">1544 год</h3>
+              <p>
+                Первое упоминание о финской деревне Сейвясте, после ставшей
+                поселком Озерки. Уже тогда крестьянам и рыбакам понравились
+                удачное расположение и уникальная природа этого места.{" "}
+              </p>
+            </div>
+            <div className={style.flex_item}>
+              <div className={style.img_container}>
+                <img src="/history_1.png" alt="" />
+              </div>
+            </div>
+            <div className={style.flex_item}>
+              <div className={style.img_container}>
+                <img src="/history_2.png" alt=""   />
+              </div>
+            </div>
+          </div>
+          <div className={style.flex_center_subflex_2}>
+            <div className={style.flex_item}>
+              <h3 className="white">1873 год</h3>
+              <p>
+                Дата основания маяка Стирсудден на Сейвястовском мысе. С его
+                появлением на Финском заливе воцарились безопасность и покой. В
+                ясные дни корабли обходят мыс, а в плохую погоду укрываются за
+                ним от шторма.
+              </p>
+            </div>
+            <div className={style.flex_item_s}>
+              <div className={style.img_container}>
+                <img src="/history_3.png" alt=""  />
+              </div>
+            </div>
+            <div className={style.flex_item}>
+              <h3 className="white">107 участков</h3>
+              <p>
+                находится на территории «Ярвино». Поселок сочетает в себе
+                чистоту и спокойствие финской деревни и современную
+                инфраструктуру. Название «Ярвино» происходит от финского Järvi —
+                озеро. Расположенное неподалеку от поселка озеро станет любимым
+                местом отдыха жителей поселка.
+              </p>
+            </div>
+          </div>
         </div>
       </Wrapper>
     </section>
