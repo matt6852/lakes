505a3932f670eb4d1de475ca86c4d2be794778b1
matt6852 <prorokwow@mail.ru>
2021-06-17 17:39:25 +0300

    added title and meta teg description

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,21 @@ import HowToBuy from "components/Sections/HowToBuy";
 import GenPlan from "components/Sections/GenPlan";
 import Contacts from "components/Sections/Contacts";
 import Footer from "components/Sections/Footer";
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>
+          Участки под строительство дома в коттеджном поселке — Ярвино
+        </title>
+        <meta
+          name="description"
+          content="Земельные участки в Озерках Выборгский район в коттеджном поселке Ярвино, 80 км от Санкт-Петербурга. Купить участок в коттеджном поселке под ИЖС на сайте и по ☎️"
+        ></meta>
+          <meta name="keywords" content="HTML, CSS, JavaScript,React,NextJS"></meta>
+      </Head>
       <Header />
       <HeaderMob />
 
